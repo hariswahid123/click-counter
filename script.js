21f22c86a332dcc390af40bcd0bac597af13f4b7
hariswahid123 <hariswahid2010@gmail.com>
2025-11-02 20:04:25 +0500

    Add  high score and more

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 let clickMe = document.getElementById("clickMe");
 let result = document.getElementById("result");
+let highScoreDisplay = document.getElementById("highScore");
 let count = 0;
 let isActive = true;
 let timer;
 
+let highScore = localStorage.getItem("highScore") || 0;
+highScoreDisplay.textContent = highScore;
 
 function startTimer() {
-  clearTimeout(timer); 
+  clearTimeout(timer);
   timer = setTimeout(() => {
     isActive = false;
     clickMe.style.backgroundColor = "gray";
     clickMe.style.cursor = "not-allowed";
     clickMe.querySelector("h1").textContent = "Time Over!";
+
+    if (count > highScore) {
+      highScore = count;
+      localStorage.setItem("highScore", highScore);
+      highScoreDisplay.textContent = highScore;
+    }
+
   }, 10000);
 }
 
 startTimer();
 
-
 clickMe.addEventListener("click", function () {
   if (!isActive) return;
   count++;
-  console.log(count);
   result.innerHTML = count;
 });
 
@@ -32,5 +40,5 @@ function reset() {
   clickMe.style.backgroundColor = "#2563eb";
   clickMe.style.cursor = "pointer";
   clickMe.querySelector("h1").textContent = "click me";
-  startTimer(); 
+  startTimer();
 }
